418ed540ac05f8a4e032460197a2d0ba22477bec
htetyeeaung37 <htetyeeaung5566@gmail.com>
2026-03-17 21:22:07 +0630

    fix: prisma 7 config and remove sqlite adapter

--- social-api/libs/prisma.ts
+++ social-api/libs/prisma.ts
@@ -1,10 +1,5 @@
-import "dotenv/config";
-import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
-import { PrismaClient } from "../generated/prisma/client";
+import { PrismaClient } from "@prisma/client";
 
-const connectionString = `${process.env.DATABASE_URL}`;
+export const prisma = new PrismaClient();
 
-const adapter = new PrismaBetterSqlite3({ url: connectionString });
-const prisma = new PrismaClient({ adapter });
-
-export { prisma };
+export default prisma;
